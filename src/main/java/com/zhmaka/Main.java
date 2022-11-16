38dfbe04ec0a5532e458a755c71297d0be3e585b
alla5149 <jmakaaa@gmail.com>
2022-11-16 18:41:19 +0200

    Create Main class

---
+++ src/main/java/com/zhmaka/Main.java
@@ -0,0 +1,26 @@
+package com.zhmaka;
+
+import com.zhmaka.arrayMethods.ArraysMethods;
+import com.zhmaka.arrayMethods.Lock;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ArraysMethods arraysMethods = new ArraysMethods();
+        int[] array = arraysMethods.getArray();
+        System.out.println();
+        arraysMethods.sortBy(array);
+        System.out.println(Arrays.toString(array));
+        System.out.println();
+        System.out.println("Возведем каждое третье число массива в куб");
+        arraysMethods.cubeTheThirdElement(array);
+        System.out.println();
+        Lock lock = new Lock(array);
+        lock.printArray();
+        lock.getArray()[0] = 10;
+        lock.printArray();
+
+
+    }
+}
---
+++ src/main/java/com/zhmaka/arrayMethods/ArraysMethods.java
@@ -0,0 +1,64 @@
+package com.zhmaka.arrayMethods;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArraysMethods {
+
+    final Scanner input = new Scanner(System.in);
+    final Random random = new Random();
+
+    public int[] getArray() {
+        System.out.println("Введите длинну масива, который будет больше или равен 10");
+        int sizeArray = input.nextInt();
+        int[] array = new int[sizeArray];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 200 - 100);
+        }
+        String intAString = Arrays.toString(array);
+        System.out.println(intAString);
+        return array;
+    }
+
+    public int sortBy(int[] array) {
+        int answer = 0;
+        System.out.println("Как вы хотыте отсортировать массив?\n 1 - сортировка по росту; \n 2 - сортировка по убыванию");
+        answer = input.nextInt();
+        if (answer == 1) {
+            sortByGrowth(array);
+        } else if (answer == 2) {
+            sortDescending(array);
+        } else {
+            System.out.println("Такого варианта сортировки нет в списку!");
+        }
+        return answer;
+    }
+
+    private int[] sortDescending(int[] array) {
+        Arrays.sort(array);
+        int[] descArray = new int[array.length];
+        for(int i=0; i<array.length; i++) {
+            descArray[i] = array[(array.length-1)-i];
+        }
+        return descArray;
+    }
+
+    public int[] sortByGrowth(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                i = -1;
+            }
+        }
+        return array;
+    }
+    public void cubeTheThirdElement(int[] array){
+        for (int i = 2; i < array.length; i += 3){
+            array[i] = array[i] * array[i] * array[i];
+        }
+    }
+}
